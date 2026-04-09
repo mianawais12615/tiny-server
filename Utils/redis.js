59a466f35b59f2f8c7f285702fe633e9554715b7
mianawais12615 <mianawais12615@gmail.com>
2026-04-09 15:50:36 +0500

    Update redis.js

--- Utils/redis.js
+++ Utils/redis.js
@@ -2,36 +2,37 @@ import { createClient } from "redis";
 import dotenv from "dotenv";
 dotenv.config();
 
-
 const redisClient = createClient({
-    username: process.env.REDIS_USERNAME,
-    password: process.env.REDIS_PASSWORD,
-    socket: {
-        host: process.env.REDIS_HOST,
-        port: Number(process.env.REDIS_PORT),
-    },
+  username: process.env.REDIS_USERNAME,
+  password: process.env.REDIS_PASSWORD,
+  socket: {
+    host: process.env.REDIS_HOST,
+    port: Number(process.env.REDIS_PORT),
+  },
 });
 
 redisClient.on("error", (err) => console.error("Redis Client Error", err));
 
 export const connectRedis = async () => {
-    if (!redisClient.isOpen) {
-        await redisClient.connect();
-    }
-    console.log("Connect to Redis")
+  if (!redisClient.isOpen) {
+    await redisClient.connect();
+  }
+  console.log("Connected to Redis.");
 };
 
-
-export const setcache = async (Key, value, exprieSeconds = 3600) => {
-    await redisClient.set(Key, JSON.stringify(value), {
-        EX: exprieSeconds,
+export const setCache = async (key, value, expireSeconds = 3600) => {
+  try {
+    await redisClient.set(key, JSON.stringify(value), {
+      EX: expireSeconds,
     });
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 export const getCache = async (key) => {
-    const data = await redisClient.get
-        (key);
-    return data ? JSON.parse(data) : null;
+  const data = await redisClient.get(key);
+  return data ? JSON.parse(data) : null;
 };
 
 export default redisClient;
