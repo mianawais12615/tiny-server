4178f2c80aa9a45d5910da76cb7fafa720acae6d
mianawais12615 <mianawais12615@gmail.com>
2026-04-09 15:28:18 +0500

    u8

--- Controllers/RedirectURL.js
+++ Controllers/RedirectURL.js
@@ -1,8 +1,19 @@
 import { URLs } from "../Models/url.js";
+import { getCache } from "../Utils/redis.js";
 
 export const RedirectURL = async (req, res) => {
   const { shortId } = req.params;
   try {
+
+    //cache  hit
+    const longURlfromCache = await getCache(shortId);
+    if(longURlfromCache){
+      res.redirect(element.longUrl);
+      return;
+    }
+
+    //cache miss
+
     const resUrls = await URLs.find({ shortId: shortId });
     const element = resUrls[0];
     console.log(element);
